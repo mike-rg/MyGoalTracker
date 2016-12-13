adaf0c3f608ab039928b3511a9351397716ef545
Miguel Maldonado <miguel.alberto.maldonado@gmail.com>
2016-12-12 22:38:28 -0300

    Campos basicos del modelo meta

--- myGoalTracker/meta/models.py
+++ myGoalTracker/meta/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from usuario.models import Usuario
 
-# Create your models here.
+
+class MetaAbstracta(models.Model):
+    abstract = True
+    titulo = models.CharField(max_length=80, default='Título')
+    descripcion = models.CharField(max_length=200, default='Descripción')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    fecha_comienzo = models.DateTimeField(null=True, blank=True)
+
+
+class Meta(MetaAbstracta):
+    usuario = models.ForeignKey(
+        Usuario, on_delete=models.CASCADE, blank=True, null=True)
